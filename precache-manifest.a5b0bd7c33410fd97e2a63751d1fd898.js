f6356f4d738b7b62ec29accfa6c3d7bd2288d534
hemantkrjain <59372128+hemantkrjain@users.noreply.github.com>
2023-07-23 00:39:27 +0530

    Updates

---
+++ precache-manifest.a5b0bd7c33410fd97e2a63751d1fd898.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c500ab74894f77ac4fbe",
+    "url": "/textutils/static/css/main.33ce4957.chunk.css"
+  },
+  {
+    "revision": "c500ab74894f77ac4fbe",
+    "url": "/textutils/static/js/main.c500ab74.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/textutils/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/textutils/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "91449b700f0ab8a2a413",
+    "url": "/textutils/static/js/runtime~main.91449b70.js"
+  },
+  {
+    "revision": "ca269eba418c39b244f6522afe552085",
+    "url": "/textutils/index.html"
+  }
+];
